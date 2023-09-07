bb09f2e28e0ca84a2c93a7ef516f8100ea7c100b
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-09-06 18:07:40 -0700

    adds lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,16 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
+    def __init__(self, url='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response=requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        messages=[]
+        message = json.loads(self.get_response_body())
+        for message in message:
+            messages.append(message)
+        return messages
